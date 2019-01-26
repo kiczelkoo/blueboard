d221299a386a2e317f653412aca2bd25c5f1d311
skltl <as118410@gmail.com>
2019-01-26 20:04:51 +0100

    initail commit with integration of springboot and angular; once a jar is created, it has angular stuff in it (but needs more work)

---
+++ src/main/java/com/shipit/blueboard/BlueboardApplication.java
@@ -0,0 +1,14 @@
+package com.shipit.blueboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlueboardApplication.class, args);
+	}
+
+}
+
