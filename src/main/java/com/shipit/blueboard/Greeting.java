4e5a3b71f8a8594e1c6fc9070ed8e4b856ce7487
Olga <olga.pozn@gmail.com>
2019-01-31 21:49:14 +0100

    initial stomp messaging

---
+++ src/main/java/com/shipit/blueboard/Greeting.java
@@ -0,0 +1,19 @@
+package com.shipit.blueboard;
+
+public class Greeting {
+
+    private String content;
+
+    public Greeting() {
+    }
+
+    public Greeting(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+
+}
